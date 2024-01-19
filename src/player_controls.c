1c0e21d811342c62ac5ef39189976581a97aca93
ibeliaie <ibeliaie@student.42wolfsburg.de>
2024-01-19 18:20:14 +0100

    added arrow key controls

--- src/player_controls.c
+++ src/player_controls.c
@@ -6,7 +6,7 @@
 /*   By: ibeliaie <ibeliaie@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 16:07:05 by ibeliaie          #+#    #+#             */
-/*   Updated: 2024/01/18 18:01:13 by ibeliaie         ###   ########.fr       */
+/*   Updated: 2024/01/19 18:19:03 by ibeliaie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,22 @@ int	player_move(int keycode, t_vars *vars)
 {
 	if (keycode == XK_Escape)
 		press_close_key(vars);
-	if (keycode == XK_w)
+	if (keycode == XK_w || keycode == XK_Up)
 	{
 		move_up(vars);
 		exit_door(vars);
 	}
-	if (keycode == XK_d)
+	if (keycode == XK_d || keycode == XK_Right)
 	{
 		move_right(vars);
 		exit_door(vars);
 	}
-	if (keycode == XK_s)
+	if (keycode == XK_s || keycode == XK_Down)
 	{
 		move_down(vars);
 		exit_door(vars);
 	}
-	if (keycode == XK_a)
+	if (keycode == XK_a || keycode == XK_Left)
 	{
 		move_left(vars);
 		exit_door(vars);
