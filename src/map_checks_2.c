872d30cc7d7b47d93a94091efd8a94bed2819aa0
ibeliaie <ibeliaie@student.42wolfsburg.de>
2024-02-01 18:02:23 +0100

    improving readability pt.4

--- src/map_checks_2.c
+++ src/map_checks_2.c
@@ -6,7 +6,7 @@
 /*   By: ibeliaie <ibeliaie@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 16:40:23 by ibeliaie          #+#    #+#             */
-/*   Updated: 2024/01/31 19:47:25 by ibeliaie         ###   ########.fr       */
+/*   Updated: 2024/02/01 18:00:25 by ibeliaie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ int	validate_map_border(t_vars *vars)
 	while (i < vars->height - 1)
 	{
 		ptr = gnl(fd);
+		if (!ptr)
+			return (0);
 		if (vars->map[0][i] != '1' || vars->map[vars->height
 			- 1][i] != '1')
 		{
@@ -44,9 +46,7 @@ int	validate_map_border(t_vars *vars)
 void	map_dimensions(t_vars *vars)
 {
 	vars->width = map_width(vars);
-	ft_printf("width: %d\n", vars->width);
 	vars->height = map_height(vars->map_name);
-	ft_printf("height: %d\n", vars->height);
 }
 
 void	validate_map(t_vars *vars)
@@ -57,13 +57,12 @@ void	validate_map(t_vars *vars)
 	validate_map_border(vars);
 	validate_map_components(vars->map);
 	validate_map_rectangular(vars, 0, 0);
+	path_finder(vars->display_x, vars->display_y, vars);
+	if (vars->counts.count_eggs == 0
+		|| vars->path.collectible != vars->counts.count_eggs)
+	{
+		ft_printf("\033[0;31merror\nimpossible to complete the game\n");
+		free_maps(vars);
+		exit(1);
+	}
 }
-
-//	path_finder(vars->display_x, vars->display_y, vars);
-//	if (vars->counts.count_eggs == 0
-//		|| vars->path.collectible_egg != vars->counts.count_eggs)
-//	{
-//		ft_printf("\033[0;31merror\nimpossible to complete the game\n");
-//		free_wrong(vars);
-//		exit(1);
-//	}
